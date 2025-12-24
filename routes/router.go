fcd1bbfa0106252d2d7f0516604d7d36eb99335e
okaka favour <okakafavour81@gmail.com>
2025-12-24 09:54:04 +0100

    commit

--- routes/router.go
+++ routes/router.go
@@ -101,9 +101,9 @@ func SetUpRoutes(r *gin.Engine) {
 	// ============================================================
 	r.POST("/signup", controllers.Register)
 	r.POST("/login", controllers.Login)
-	r.POST("/forgot-password", controllers.ForgotPassword)
+	r.POST("/auth/forgot-password", controllers.ForgotPassword)
 	r.GET("/reset-password", controllers.ResetPassword)
-	r.POST("/reset-password", controllers.ResetPassword)
+	r.POST("/auth/reset-password", controllers.ResetPassword)
 	r.GET("/test-email", controllers.TestEmail)
 
 	// ============================================================
